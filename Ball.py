16cd0bc870df02c3f4903a6493b6f22795a576a0
KAryal10 <147074025+KAryal10@users.noreply.github.com>
2024-03-05 10:26:54 -0500

    Created ball class

---
+++ Ball.py
@@ -0,0 +1,41 @@
+import pygame
+
+class Ball:
+  def __init__ (self,xPos,yPos, radius, speed, color):
+    self.xPos, self.yPos, self.radius = xPos, yPos, radius
+    self.speed = speed
+    self.color= color
+    self.xFac, self.yFac = 1 , 1
+    self.ball = pygame.draw.circle(screen, self.color, (self.xPos,self.yPos), self.radius)
+
+  def display(self):
+    self.ball = pygame.draw.circle(screen, self.color, (self.xPos,self.yPos), self.radius)
+
+  def update(self):
+    self.xPos += self.xFac*self.speed
+    self.yPos += self.yFac*self.speed
+
+    #setting vertical boundary on the screen for the ball
+    if self.xPos <=0 or self.xPos >= WIDTH:
+      self.xFac *= -1
+
+    #setting upper boundary on the creen for the ball
+    if self.yPos<=0:
+      self.yFac *= -1
+
+    if self.yPos >= HEIGHT:
+      return True
+
+    return False
+
+  def reset(self):
+    self.xPos = WIDTH
+    self.yPos = HEIGHT
+    self.xFac, self.yFac = -1,-1
+
+  def hit(self):
+    self.yFac *= -1
+
+  def getRect(self)
+    return self.ball
+      
